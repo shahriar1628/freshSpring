361e185862f922abd2333a9adfbf723edd27745e
shahriar1628 <shahriarcsedu@gmail.com>
2018-09-28 11:02:37 +0600

    full java configuration spring

---
+++ FreshSpring/src/main/java/com/spring/config/DispatcherConfig.java
@@ -0,0 +1,23 @@
+package com.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@ComponentScan(basePackages = {"com.spring.controller"})
+public class DispatcherConfig {
+    @Bean
+    public InternalResourceViewResolver  internalResourceViewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setViewClass(JstlView.class);
+        internalResourceViewResolver.setPrefix("/WEB-INF/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return  internalResourceViewResolver;
+    }
+
+}
--- FreshSpring/src/main/java/com/spring/config/MyWebAppInitializer.java
+++ FreshSpring/src/main/java/com/spring/config/MyWebAppInitializer.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletRegistration;
 
 import org.springframework.core.annotation.Order;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -17,12 +18,11 @@ import javax.servlet.ServletContext;
 public class MyWebAppInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext container) {
-        XmlWebApplicationContext context = new XmlWebApplicationContext();
-        context.setConfigLocation("/WEB-INF/spring/dispatcher-config.xml");
-
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("com.spring.config");
+        container.addListener(new ContextLoaderListener(context));
         ServletRegistration.Dynamic dispatcher = container
                 .addServlet("dispatcher", new DispatcherServlet(context));
-
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
     }
