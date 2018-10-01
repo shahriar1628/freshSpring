506353ce9760b39ff4e830db5bd6b7bd95f92d01
shahriar1628 <shahriarcsedu@gmail.com>
2018-10-01 11:03:28 +0600

    implement WebMvcConfigurer and add addViewContorller

--- FreshSpring/src/main/java/com/spring/config/DispatcherConfig.java
+++ FreshSpring/src/main/java/com/spring/config/DispatcherConfig.java
@@ -4,13 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceView;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = {"com.spring.controller"})
-public class DispatcherConfig {
+public class DispatcherConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/start").setViewName("start");
+    }
     @Bean
     public InternalResourceViewResolver  internalResourceViewResolver(){
         InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
